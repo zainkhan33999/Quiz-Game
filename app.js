75a24d55d63152cef52eb10a9d8d1cec9bc86894
zainkhan <khanzain33999@gmail.com>
2023-09-07 09:59:52 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,167 @@
+
+const questions = [
+    {
+        question: "Which is the largest animal in the world?",
+        answers: [
+            { text: "Shark", correct: false },
+            { text: "Blue Whale", correct: true },
+            { text: "Elephant", correct: false },
+            { text: "Giraffes", correct: false },
+        ],
+    },
+    {
+        question: "What is the capital of France?",
+        answers: [
+            { text: "Berlin", correct: false },
+            { text: "Madrid", correct: false },
+            { text: "Rome", correct: false },
+            { text: "Paris", correct: true },
+        ],
+    },
+    {
+        question: "Which planet is known as the Red Planet?",
+        answers: [
+            { text: "Earth", correct: false },
+            { text: "Mars", correct: true },
+            { text: "Venus", correct: false },
+            { text: "Jupiter", correct: false },
+        ],
+    },
+    {
+        question: "What is the chemical symbol for water?",
+        answers: [
+            { text: "H2O", correct: true },
+            { text: "CO2", correct: false },
+            { text: "O2", correct: false },
+            { text: "NaCl", correct: false },
+        ],
+    },
+    {
+        question: "Which gas do plants absorb from the atmosphere?",
+        answers: [
+            { text: "Oxygen", correct: false },
+            { text: "Carbon Dioxide", correct: true },
+            { text: "Nitrogen", correct: false },
+            { text: "Hydrogen", correct: false },
+        ],
+    },
+    {
+        question: "Who wrote the play 'Romeo and Juliet'?",
+        answers: [
+            { text: "William Shakespeare", correct: true },
+            { text: "Jane Austen", correct: false },
+            { text: "Charles Dickens", correct: false },
+            { text: "F. Scott Fitzgerald", correct: false },
+        ],
+    },
+    {
+        question: "What is the largest mammal on land?",
+        answers: [
+            { text: "African Elephant", correct: true },
+            { text: "Grizzly Bear", correct: false },
+            { text: "Lion", correct: false },
+            { text: "Giraffe", correct: false },
+        ],
+    },
+    {
+        question: "In which year did Christopher Columbus first reach the Americas?",
+        answers: [
+            { text: "1492", correct: true },
+            { text: "1776", correct: false },
+            { text: "1620", correct: false },
+            { text: "1865", correct: false },
+        ],
+    },
+    {
+        question: "What is the smallest planet in our solar system?",
+        answers: [
+            { text: "Earth", correct: false },
+            { text: "Mars", correct: false },
+            { text: "Mercury", correct: true },
+            { text: "Venus", correct: false },
+        ],
+    },
+];
+
+const questionElement = document.querySelector("#question");
+const answerButton = document.querySelector(".answer-buttons");
+const nextButton = document.querySelector(".next-button");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion() {
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + "." + currentQuestion.question;
+
+    currentQuestion.answers.forEach((answer) => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButton.appendChild(button);
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+}
+
+function resetState() {
+    nextButton.style.display = "none";
+    while (answerButton.firstChild) {
+        answerButton.removeChild(answerButton.firstChild);
+    }
+}
+
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if (isCorrect) {
+        selectedBtn.classList.add("correct");
+        score++;
+    } else {
+        selectedBtn.classList.add("Incorrect");
+    }
+    Array.from(answerButton.children).forEach((button) => {
+        if (button.dataset.correct === "true") {
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+
+function showScore() {
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton() {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+        showQuestion();
+    } else {
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", () => {
+    if (currentQuestionIndex < questions.length) {
+        handleNextButton();
+    } else {
+        startQuiz();
+    }
+});
+
+startQuiz();
